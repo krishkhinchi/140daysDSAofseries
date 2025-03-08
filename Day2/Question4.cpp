f80df777dc3b17c4960f31559147801d5054cf67
krishhackz <krishhackz.in@gmail.com>
2025-03-09 02:17:07 +0530

    Type Casting

---
+++ Day2/Question4.cpp
@@ -0,0 +1,17 @@
+// Type Casting.
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int Test1 = 'a';
+    cout << "The ASCII value of 'a' is: " << Test1 << endl;
+
+    char Test2 = 98;
+    cout << "The character represented by ASCII value 98 is: " << Test2 << endl;
+
+    char Test3 = 123456;
+    cout << "The character represented by ASCII value 123456 is: " << Test3 << endl;
+
+    return 0;
+}
