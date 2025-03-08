6075f5cba07441c14fa88366f2c20a12e1cbf154
krishhackz <krishhackz.in@gmail.com>
2025-03-09 02:20:02 +0530

    Unsigned

---
+++ Day2/Question5.cpp
@@ -0,0 +1,14 @@
+// Unsigned.
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    unsigned int x = 10;
+    cout << "Value of x: " << x << endl;
+
+    unsigned int y = -10;
+    cout << "Value of y: " << y << endl;
+
+    return 0;
+}
