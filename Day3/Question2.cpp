fa8b5ab132415c617ac4d95e3f2b5f09de005360
krishhackz <krishhackz.in@gmail.com>
2025-03-09 02:40:14 +0530

    number is positive or negative

---
+++ Day3/Question2.cpp
@@ -0,0 +1,19 @@
+// Check a number is positive or negative.
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num>0){
+        cout << "The number is positive.";
+    }
+    else{
+        cout << "The number is negative.";
+    }
+
+    return 0;
+}
