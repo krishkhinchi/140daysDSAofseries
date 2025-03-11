f207cd32ae01274b60fdac1a18a0c3352f7143a2
krishhackz <krishhackz.in@gmail.com>
2025-03-11 11:27:11 +0530

    Alpha Triangle Pattern #2

---
+++ Day4/Question11.cpp
@@ -0,0 +1,20 @@
+// A 
+// B C
+// D E F
+// G H I J
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    char ch = 'A';
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=i;j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
