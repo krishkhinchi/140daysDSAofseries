94c648533cddf8df7ff55c1faa47272d0b04829b
krishhackz <krishhackz.in@gmail.com>
2025-03-11 13:27:43 +0530

    Alpha Triangle Pattern #3

--- Day4/Question12.cpp
+++ Day4/Question12.cpp
@@ -1,21 +1,19 @@
-// A 
-// B C
-// C D E
-// D E F G
+// D 
+// C D
+// B C D
+// A B C D
 
 #include <iostream>
 using namespace std;
 int main(){
 
-    char ch = 'A';
-    char temp;
+    char ch;
+
     for(int i=0;i<4;i++){
-        temp = ch;
         for(int j=0;j<=i;j++){
-            cout << temp << " ";
-            temp++;
+            ch = 68 - i + j;
+            cout << ch << " ";
         }
-        ch++;
         cout << endl;
     }
 
