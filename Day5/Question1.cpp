fbbec689d7f2b56ec00a7772934018857e4c6e30
krishhackz <krishhackz.in@gmail.com>
2025-03-13 17:03:06 +0530

    Bitwise Operator

---
+++ Day5/Question1.cpp
@@ -0,0 +1,16 @@
+// Bitwise Operator
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a=5;
+    int b=10;
+
+    cout << "a&b = " << (a&b) << endl;
+    cout << "a|b = " << (a|b) << endl;
+    cout << "a^b = " << (a^b) << endl;
+    cout << "~a = " << ~a << endl;
+    
+    return 0;
+}
