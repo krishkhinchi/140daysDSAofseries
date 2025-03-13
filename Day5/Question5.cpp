0e656cb316f405951fef37b7cb10b7a4bfad762a
krishhackz <krishhackz.in@gmail.com>
2025-03-13 17:29:45 +0530

    Guess the output #2

---
+++ Day5/Question5.cpp
@@ -0,0 +1,18 @@
+// Guess the output #2
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a = 1;
+    int b = 2;
+
+    if (a-- > 0 || ++b > 2){
+        cout << "Stage1 - Inside If ";
+    } else{
+        cout << "Stage2 - Inside else ";
+    }
+    cout << a << " " << b << endl;
+
+    return 0;
+}
