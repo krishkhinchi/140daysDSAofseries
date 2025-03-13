cc5a70cae20714568884f6a6781530c23ed30e24
krishhackz <krishhackz.in@gmail.com>
2025-03-13 17:34:25 +0530

    Guess the output #4

---
+++ Day5/Question7.cpp
@@ -0,0 +1,14 @@
+// Guess the output #4
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a = 1;
+    int b = a++;
+    int c = ++a;
+    cout << b ;
+    cout << c ;
+
+    return 0;
+}
