8fc49439cbac2ec6eb4dde60e1d3071278c118d3
krishhackz <krishhackz.in@gmail.com>
2025-03-14 11:55:47 +0530

    Guess the output #4

---
+++ Day5/Question15.cpp
@@ -0,0 +1,14 @@
+// Guess the output #4
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    for(int i=0;i<5;i++){
+        for(int j=i;j<=5;j++){
+            cout << i << " " << j << endl;
+        }
+    }
+
+    return 0;
+}
