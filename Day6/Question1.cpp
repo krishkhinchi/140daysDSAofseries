96f34135eb79315720432f9591f5d901243720ad
krishhackz <krishhackz.in@gmail.com>
2025-03-15 16:27:56 +0530

    Binary representation of the input

---
+++ Day6/Question1.cpp
@@ -0,0 +1,23 @@
+// Binary representation of the input
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+    int input,value=0;
+    cout << "Enter a value: ";
+    cin >> input;
+
+    int i=0;
+    while(input>0){
+        int bit = input & 1;
+        value = (bit * pow(10,i))+value;
+        input = input >> 1;
+        i++;
+    }
+
+    cout << "Binary representation of the input: " << value;
+
+    return 0;
+}
