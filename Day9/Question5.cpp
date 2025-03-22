258459908938e7aa900786521da157f8f5ec7ae1
krishhackz <krishhackz.in@gmail.com>
2025-03-22 19:50:23 +0530

    Reverse an array by function

---
+++ Day9/Question5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n){
+    for(int i=0;i<n; i++){
+        cout << " " << arr[n-i-1];
+    }
+}
+
+int main(){
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    int length=5;
+
+    reverseArray(arr, length);
+
+    return 0;
+}
