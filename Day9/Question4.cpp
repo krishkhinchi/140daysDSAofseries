d6358df92d3a22e922f3aaf22be475f840b7cf8e
krishhackz <krishhackz.in@gmail.com>
2025-03-22 18:59:15 +0530

    Check element exist in array or not by function

---
+++ Day9/Question4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool checkExist(int arr[],int n){
+    for(int i=0;i<10;i++){
+        if(arr[i]==n){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int array[10] = {4, -5, 3, 6, 26, 1, 7, 2, 9, 0};
+    bool Exist = checkExist(array, num);
+    (Exist) ? cout << num << " exists in the array" : cout << num <<  " does not exist in the array";
+
+    return 0;
+}
