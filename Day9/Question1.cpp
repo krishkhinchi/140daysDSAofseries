db1c7407f2cd92f34ed460505dcf278e9d52a4e6
krishhackz <krishhackz.in@gmail.com>
2025-03-22 12:57:50 +0530

    Print an array using function

---
+++ Day9/Question1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int* array, int size){
+    for(int i=0;i<size;i++){
+        cout << array[i] << endl;
+    }
+}
+
+int main(){
+
+    int arr[5]={1,5,3,4,2};
+    int size = sizeof(arr)/sizeof(int);
+    printArray(arr, size);
+
+    return 0;
+}
