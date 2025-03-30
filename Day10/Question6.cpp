9f6fe34b1c91d395979885180bf15ee09a58902f
krishhackz <krishhackz.in@gmail.com>
2025-03-30 20:15:00 +0530

    Pair Sum (code360)

---
+++ Day10/Question6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+    int s =5;
+    int arr[4] = {1,2,3,4,5};
+    vector<vector<int>> value;
+   for(int i=0;i<5;i++){
+      for(int j=i+1;j<5;j++){
+         if(arr[i]+arr[j]==s){
+            vector<int>store;
+            store.push_back(min(arr[i],arr[j]));
+            store.push_back(max(arr[i],arr[j]));
+            value.push_back(store);
+         }
+      }
+   }
+   cout << value.size();
+
+    return 0;
+}
