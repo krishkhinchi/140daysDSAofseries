88834b3fdf63bde187d144ea1c60c72da611d15f
krishhackz <krishhackz.in@gmail.com>
2025-03-30 20:34:37 +0530

    Find All Triplets With Zero Sum (code360)

---
+++ Day10/Question8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    int n=7;
+    vector<int>value;
+    for(int i=0;i<7;i++){
+        for(int j=i+1;j<7;j++){
+            for(int k=j+1;k<7;k++){
+                if(arr[i]+arr[j]+arr[k]==n){
+                    vector<int>store;
+                    store.push_back(min(arr[i],arr[j],arr[k]));
+                    store.push_back(max(arr[i],arr[j],arr[k]));
+                    value.push_back(store);
+                 }
+            }
+        }
+    }
+    cout << value.size();
+
+    return 0;
+}
