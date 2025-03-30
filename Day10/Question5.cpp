254e52d1adb62f56edd031004d41a700526d5602
krishhackz <krishhackz.in@gmail.com>
2025-03-30 19:54:17 +0530

    Intersection Of Two Sorted Arrays (code360)

---
+++ Day10/Question5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+    int n=5,m=5;
+    int arr1[5] = {1,2,3,4,5};
+    int arr2[5] = {6,7,8,9,10};
+    int i=0,j=0;
+	vector<int> vec;
+	while(i<n && j<m){
+		if(arr1[i]==arr2[j]){
+			vec.push_back(arr1[i]);
+			i++;
+			j++;
+		}
+		else if(arr1[i]<arr2[j]){
+			i++;
+		}
+		else{
+			j++;
+		}
+	}
+    cout << vec.size();
+
+    return 0;
+}
