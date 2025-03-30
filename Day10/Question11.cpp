87eb0f9f5da5e962706eb7ec50e17eb977956c29
krishhackz <krishhackz.in@gmail.com>
2025-03-30 21:04:14 +0530

    time, space complexity #3

---
+++ Day10/Question11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a=0,N=10;
+    for (int i = 0; i < N; i++){
+        for (int j = N; j > i; j--)
+        {
+            a = a + i + j;
+        }
+    }
+
+    return 0;
+}
