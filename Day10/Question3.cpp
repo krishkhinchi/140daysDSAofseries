c439174fb5758f06052d4853cda10ef4701554d3
krishhackz <krishhackz.in@gmail.com>
2025-03-30 18:59:06 +0530

    1207. Unique Number of Occurrences (leetcode)

--- Day10/Question3.cpp
+++ Day10/Question3.cpp
@@ -5,37 +5,44 @@
 #include <map>
 using namespace std;
 
-class Solution {
-    public:
-        bool uniqueOccurrences(vector<int>& arr) {
-            vector<int>vec;
-    
-            map<int, int>mp;
-            for(int i=0 ; i<arr.size() ; i++)
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        vector<int> vec;
+
+        map<int, int> mp;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            mp[arr[i]]++;
+        }
+        for (auto m : mp)
+        {
+            cout << m.first << " " << m.second;
+            vec.push_back(m.second);
+            cout << "\n";
+        }
+
+        for (int i = 0; i < vec.size(); i++)
+        {
+            for (int j = i + 1; j < vec.size(); j++)
             {
-                mp[arr[i]]++;
-            }
-            for(auto m:mp){
-                cout<<m.first<<" "<<m.second;
-                vec.push_back(m.second);
-                cout<<"\n";
-            }
-    
-            for(int i=0;i<vec.size();i++){
-                for(int j=i+1;j<vec.size();j++){
-                    if(vec[i]==vec[j]){
-                        return false;
-                    }
+                if (vec[i] == vec[j])
+                {
+                    return false;
                 }
             }
+        }
         return true;
     }
 };
 
-int main(){
+int main()
+{
 
     vector<int> arr;
-    arr[5]={1,2,3,4,5};
+    arr[5] = {1, 2, 3, 4, 5};
 
     Solution s;
     s.uniqueOccurrences(arr);
