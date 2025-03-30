6d5ca3fed07bdc28d0bf6f6aa742c355b09c194f
krishhackz <krishhackz.in@gmail.com>
2025-03-30 23:38:46 +0530

    Implimentation Binary Search

---
+++ Day12/Question1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int Binary(int arr[], int length, int key){
+    int low = 0, high = length - 1;
+    int mid = low+high/2;
+
+    while(high>low){
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(key>arr[mid]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    int even[6] = {4,6,8,2,10,4};
+    // int odd[6] = {1,3,5,7,9,1};
+    int key = 4;
+
+    cout << key << "is at index number: " << Binary(even, 6, key) << endl;
+
+    return 0;
+}
