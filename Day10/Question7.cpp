fa8a866a8ce4783ce091a102eb4aece199fce543
krishhackz <krishhackz.in@gmail.com>
2025-03-30 20:21:52 +0530

    Sort 0 1 2 (code360)

--- Day10/Question7.cpp
+++ Day10/Question7.cpp
@@ -2,29 +2,35 @@
 
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
     int arr[6] = {1, 0, 0, 2, 1, 1};
-    int n=6;
-    int left = 0, right = n-1;
-   while(left<=right){
-      while(arr[left]==0 && left<right){
-         left++;
-      }
-      while(arr[right]==1 && left<right){
-         right--;
-      }
+    int n = 6;
+    int left = 0, right = n - 1;
+    while (left <= right)
+    {
+        while (arr[left] == 0 && left < right)
+        {
+            left++;
+        }
+        while (arr[right] == 1 && left < right)
+        {
+            right--;
+        }
 
-      if(left<right){
-         swap(arr[left],arr[right]);
-         left++;
-         right--;
-      }
-   }
+        if (left < right)
+        {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
 
-   for(int i=0;i<n;i++){
-      cout << arr[i] << " ";
-   }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
